f37d630de79994723369615c4a86bbaca674aab3
gsap <gaetan.sap@gmail.com>
2022-06-12 17:43:20 +0200

    before merge in the same file

--- inc/connect4.h
+++ inc/connect4.h
@@ -6,7 +6,7 @@
 /*   By: gsap <gsap@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 11:04:51 by gsap              #+#    #+#             */
-/*   Updated: 2022/06/12 16:03:30 by gsap             ###   ########.fr       */
+/*   Updated: 2022/06/12 17:24:24 by gsap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # include <string.h>
 # include <stdio.h>
 # include <time.h>
+# include "../minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx_int.h"
 
 typedef struct s_grid
 {
@@ -33,57 +35,6 @@ typedef struct s_aiVal{
 	int	second;
 	int	third;
 } t_aiVal;
-
-//	game.c
-int		pickPlayer(void);
-void	startGame(t_grid *grid);
-int		mapFull(t_grid const grid);
-int		endOfGame(t_grid const grid, int const action);
-int		someoneWin(t_grid const grid, int const action);
-
-//	checkWin.c
-int		checkIfWin(char const pion, t_grid const grid, int const x, int const y);
-int		checkWinLigne(char const pion, t_grid const grid, int const x, int const y);
-int		checkWinColumn(char const pion, t_grid const grid, int const x, int const y);
-int		checkWinD1(char const pion, t_grid const grid, int const x, int const y);
-int		checkWinD2(char const pion, t_grid const grid, int const x, int const y);
-
-//	player.c
-void	playerAction(t_grid *grid, int const action);
-void	botAction(t_grid *grid, int const action);
-int		askPlayer(t_grid const grid);
-int		isInt(char const *input);
-int		isInGrid(char const *input, t_grid const grid);
-
-//	init.c
-void	initGrid(t_grid **grid, int line, int column);
-void	freeGrid(t_grid *grid);
-
-//	display.c
-void	displayStart(void);
-void	displayGrid(t_grid const grid);
-void	displayTurn(int	const turn);
-void	displayEndOfGame(t_grid const grid, int const action, int const turn);
-
-//	error.c
-int		errorArgNbr(void);
-int		errorArgVal(void);
-int		errorMalloc(void);
-
-//ia.c
-int	getBestAction(t_grid const *grid, int const player);
-void	evalWholeGrid(t_grid const *grid, int const player);
-int		eval(t_grid const *grid, int const action, int const player);
-t_aiVal	possibleAlign(t_grid const * grid, int const action, int const player, int const direction);
-void	decrementPos(int *x, int *y, int const direction);
-void	incrementPos(int *x, int *y, int const direction);
-
-/********************/
-/*		Bonus		*/
-/********************/
-
-# include "../minilibx-linux/mlx.h"
-# include "../minilibx-linux/mlx_int.h"
 
 typedef struct s_imge
 {
@@ -106,14 +57,66 @@ typedef struct s_mlx
 	int		turn;
 }	t_mlx;
 
+//	game.c
+int		pickPlayer(void);
+void	startGame(t_grid *grid);
+int		mapFull(t_grid const grid);
+int		endOfGame(t_grid const grid, int const action);
+int		someoneWin(t_grid const grid, int const action);
+/*	Bonus	*/
+void	startGameBonus(t_mlx *data);
 
-//	error.c
-int		errorMlx(void);
+//	checkWin.c
+int		checkIfWin(char const pion, t_grid const grid, int const x, int const y);
+int		checkWinLigne(char const pion, t_grid const grid, int const x, int const y);
+int		checkWinColumn(char const pion, t_grid const grid, int const x, int const y);
+int		checkWinD1(char const pion, t_grid const grid, int const x, int const y);
+int		checkWinD2(char const pion, t_grid const grid, int const x, int const y);
+
+//	player.c
+void	playerAction(t_grid *grid, int const action);
+void	botAction(t_grid *grid, int const action);
+int		askPlayer(t_grid const grid);
+int		isInt(char const *input);
+int		isInGrid(char const *input, t_grid const grid);
 
 //	init.c
+void	initGrid(t_grid **grid, int line, int column);
+void	freeGrid(t_grid *grid);
+/*	Bonus	*/
 int		initMlx(t_mlx **data, int const column, int const line, t_grid *grid);
 int		initImg(t_mlx *data);
 void	freeMlx(t_mlx *data);
+
+//	display.c
+void	displayStart(void);
+int		lenInt(int column);
+void	displayGrid(t_grid const grid);
+void	displayColHelp(t_grid const grid);
+void	displayTurn(int	const turn);
+void	displayEndOfGame(t_grid const grid, int const action, int const turn);
+/*	Bonus	*/
+void	displayInterface(t_mlx *data);
+void	displayHelp(void);
+
+//	error.c
+int		errorArgNbr(void);
+int		errorArgVal(void);
+int		errorMalloc(void);
+/*	Bonus	*/
+int		errorMlx(void);
+
+//ia.c
+int	getBestAction(t_grid const *grid, int const player);
+void	evalWholeGrid(t_grid const *grid, int const player);
+int		eval(t_grid const *grid, int const action, int const player);
+t_aiVal	possibleAlign(t_grid const * grid, int const action, int const player, int const direction);
+void	decrementPos(int *x, int *y, int const direction);
+void	incrementPos(int *x, int *y, int const direction);
+
+/********************/
+/*		Bonus		*/
+/********************/
 
 //	mlx.c
 int		keyboard(int keycode, t_mlx *data);
@@ -122,12 +125,5 @@ void	moveRight(t_mlx *data);
 int		closeGame(t_mlx *data);
 void	playerTurn(t_mlx *data);
 void	IATurn(t_mlx *data);
-
-//	game.c
-void	startGameBonus(t_mlx *data);
-
-//	display.c
-void	displayInterface(t_mlx *data);
-void	displayHelp(void);
 
 #endif
--- srcs_bonus/display.c
+++ srcs_bonus/display.c
@@ -6,7 +6,7 @@
 /*   By: gsap <gsap@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 11:04:24 by gsap              #+#    #+#             */
-/*   Updated: 2022/06/12 16:55:25 by gsap             ###   ########.fr       */
+/*   Updated: 2022/06/12 17:42:45 by gsap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,22 @@ void	displayStart(void) {
 	ft_putstr("\n\n\n");
 }
 
+//Calcul la taille des espaces entres les points pour que l'aide colle avec l'affichage
+int	lenInt(int column) {
+	int compt = 1;
+	
+	while (column / 10 > 0)
+	{
+		column = column / 10;
+		compt++;
+	}
+	return (compt);
+}
+
 //Affiche la grille dans le terminal
 void	displayGrid(t_grid const grid) {
+	int size = lenInt(grid.column);
+
 	for(int j = 0; j < grid.line; j++)
 	{
 		for(int i = 0; i < grid.column; i++)
@@ -36,28 +50,43 @@ void	displayGrid(t_grid const grid) {
 				ft_putstr("\e[91m");
 			ft_putchar(grid.map[i][j]);
 			ft_putstr("\e[39m");
-			ft_putchar(' ');
+			for(int k = 0; k < size; k++)
+				ft_putchar(' ');
 		}
 		ft_putchar('\n');
 	}
-	/*
-	**	A revoir
-	*/
+	displayColHelp(grid);
+}
+
+//Affiche les colonnes sur le terminal
+void	displayColHelp(t_grid const grid) {
+	int	size = lenInt(grid.column);
+
 	for (int i = 0; i < grid.column - 1; i++)
-		ft_putstr("--");
+	{
+		if (size == 1)
+			ft_putstr("--");
+		else if (size == 2)
+			ft_putstr("---");
+	}
 	ft_putstr("-\n");
 	for (int i = 0; i < grid.column; i++)
 	{
 		ft_putnbr(i + 1);
+		int	j = lenInt(i + 1);
+		while (j++ < size)
+			ft_putchar(' ');
 		ft_putchar(' ');
 	}
 	ft_putstr("\n");
 	for (int i = 0; i < grid.column - 1; i++)
-		ft_putstr("--");
+	{
+		if (size == 1)
+			ft_putstr("--");
+		else if (size == 2)
+			ft_putstr("---");
+	}
 	ft_putstr("-\n");
-	/*
-	** A revoir
-	*/
 }
 
 //Affiche le tour actuelle
