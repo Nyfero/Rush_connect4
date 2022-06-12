cdbde9dafb1f8c4981fac61011a0b36135829c02
gsap <gaetan.sap@gmail.com>
2022-06-12 20:53:05 +0200

    corr error arg

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: gsap <gsap@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 11:04:30 by gsap              #+#    #+#             */
-/*   Updated: 2022/06/12 20:41:13 by gsap             ###   ########.fr       */
+/*   Updated: 2022/06/12 20:52:45 by gsap             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ long long	ft_strtoll(const char *str, char **end)
 }
 
 int	errorArgNbr(void) {
-	ft_putstr_fd("The grid size must be taken as parameters to the program + an argument to activate or not the graphical interface.\nEx: ./connect4_bonus 8 10 1\n", 2);
+	ft_putstr_fd("The grid size must be taken as parameters to the program.\nEx: ./connect4 8 10\n", 2);
 	return (1);
 }
 
